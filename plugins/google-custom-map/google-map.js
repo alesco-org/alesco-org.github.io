4f994ba4c52b20266f272c54c6c7bc716731b2d7
Nicola Ferraro <ni.ferraro@gmail.com>
2017-03-23 15:59:48 +0100

    added map

--- plugins/google-custom-map/google-map.js
+++ plugins/google-custom-map/google-map.js
@@ -2,57 +2,61 @@ var map;
 
 function initMap() {
 
-    var latlng = new google.maps.LatLng(53.385873, -1.471471);
-
-    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-    var beachMarker = new google.maps.Marker({
-        position: {lat: 53.385873, lng: -1.471471},
-        map: map,
-        icon: image
-    });
+    var latlng = new google.maps.LatLng(41.8837625, 12.4872503);
 
     var styles = [
-    {
-        featureType: "landscape",
-        stylers: [
-        { color: '#eeddee' }
-        ]
-    },{
-        featureType: "natural",
-        stylers: [
-        { hue: '#ff0000' }
-        ]
-    },{
-        featureType: "road",
-        stylers: [
-        { hue: '#5500aa' },
-        { saturation: -70 }
-        ]
-    },{
-        featureType: "building",
-        elementType: "labels",
-        stylers: [
-        { hue: '#000066' }
-        ]
-    },{
-        featureType: "poi", //points of interest
-        stylers: [
-        { hue: '#0044ff' }
-        ]
-    }
+        {
+            featureType: "landscape",
+            stylers: [
+                { color: '#eeddee' }
+            ]
+        },{
+            featureType: "natural",
+            stylers: [
+                { hue: '#ff0000' }
+            ]
+        },{
+            featureType: "road",
+            stylers: [
+                { hue: '#5500aa' },
+                { saturation: -70 }
+            ]
+        },{
+            featureType: "building",
+            elementType: "labels",
+            stylers: [
+                { hue: '#000066' }
+            ]
+        },{
+            featureType: "poi", //points of interest
+            stylers: [
+                { hue: '#0044ff' }
+            ]
+        }
     ];
 
-
     var myOptions = {
-        zoom: 14,
+        zoom: 16,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
+        disableDefaultUI: false,
         styles: styles,
         scrollwheel:  false
     };
 
     map = new google.maps.Map(document.getElementById('map'), myOptions);
+
+    var beachMarker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Asilo Nido",
+        animation: google.maps.Animation.BOUNCE
+    });
+
+
+
+
+
 }
 
 
