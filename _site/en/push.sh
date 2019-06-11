1e6a716e1513bd0fd296f7a2c0cd60fd35956d86
nferraro <ni.ferraro@gmail.com>
2019-06-12 01:01:13 +0200

    fix sitemap

--- _site/en/push.sh
+++ _site/en/push.sh
@@ -2,6 +2,11 @@
 rm -rf _site
 bundle exec jekyll build
 
+# fix bug in sitemap generation
+for f in $(find . -name sitemap.xml); do
+    sed -i "s/<loc>\//<loc>/" $f
+done
+
 git add _site/*
 git commit -m "updating site"
 
