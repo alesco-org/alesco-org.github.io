734f4c13dfbe36d72206c894626d3400ce1caa17
nferraro <ni.ferraro@gmail.com>
2019-06-11 08:45:16 +0200

    updating site

---
+++ _site/push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf _site
+bundle exec jekyll build
+
+git add _site/*
+git commit -m "updating site"
+
+# requires the git-subtree package
+git subtree push --prefix _site origin gh-pages
