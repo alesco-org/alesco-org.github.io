c20e25c93c9ff806a7306a57aa607999ab3e17d5
nferraro <ni.ferraro@gmail.com>
2019-06-12 00:51:46 +0200

    sitemap fix

--- _site/push.sh
+++ _site/push.sh
@@ -2,8 +2,15 @@
 rm -rf _site
 bundle exec jekyll build
 
-git add _site/*
-git commit -m "updating site"
+en_sitemap=$(cat _site/en/sitemap.xml | grep -v urlset | grep -v "<?xml")
+sitemap=$(cat _site/sitemap.xml | grep -v "</urlset")
+
+echo $sitemap > _site/sitemap.xml
+echo $en_sitemap >> _site/sitemap.xml
+echo "</urlset>" >> _site/sitemap.xml
+
+#git add _site/*
+#git commit -m "updating site"
 
 # requires the git-subtree package
-git subtree push --prefix _site origin master
+#git subtree push --prefix _site origin master
--- push.sh
+++ push.sh
@@ -2,6 +2,13 @@
 rm -rf _site
 bundle exec jekyll build
 
+en_sitemap=$(cat _site/en/sitemap.xml | grep -v urlset | grep -v "<?xml")
+sitemap=$(cat _site/sitemap.xml | grep -v "</urlset")
+
+echo $sitemap > _site/sitemap.xml
+echo $en_sitemap >> _site/sitemap.xml
+echo "</urlset>" >> _site/sitemap.xml
+
 git add _site/*
 git commit -m "updating site"
 
