74690b63b1dd5a6a624e7e565be7c3fd431c7459
nicolaferraro <ni.ferraro@gmail.com>
2020-09-03 15:55:44 +0200

    updating site

--- _site/push.sh
+++ _site/push.sh
@@ -11,4 +11,4 @@ git add _site/*
 git commit -m "updating site"
 
 # requires the git-subtree package
-git subtree push --prefix _site origin master
+git subtree push --split --prefix _site origin master
